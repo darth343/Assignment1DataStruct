470cb1860a9b87eeca20e11f2f3f015a416e812c
darth343 <darth343@gmail.com>
2015-12-13 23:22:21 +0800

    Vec2D Class

---
+++ Assignment1/Assignment1/Vec2D.cpp
@@ -0,0 +1,10 @@
+#include "Vec2D.h"
+
+
+Vec2D::Vec2D() : x(0), y(0)
+{
+}
+
+Vec2D::Vec2D(double funcX, double funcY) : x(funcX), y(funcY)
+{
+}
---
+++ Assignment1/Assignment1/Vec2D.h
@@ -0,0 +1,13 @@
+#ifndef VEC2D_H
+#define VEC2D_H
+
+class Vec2D
+{
+public:
+	double x;
+	double y;
+	Vec2D();
+	Vec2D(double funcX, double funcY);
+};
+
+#endif // !VEC2D_H
